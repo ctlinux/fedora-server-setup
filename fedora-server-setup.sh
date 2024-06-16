7266e93022e3be3a34eb8daeef96a2500ed3cc49
ctlinux <renegadext81@gmail.com>
2024-06-16 15:22:31 -0400

    Update hostname command with hostnamectl

--- fedora-server-setup.sh
+++ fedora-server-setup.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
+# Ensure script is run as root
+if [ "$(id -u)" -ne 0 ]; then echo "Please run as root." >&2; exit 1; fi
+
 # Change hostname
 echo "### Changing hostname ###"
 sleep 2
-echo "Enter the name of the workstation"
-sudo read hostname
-sudo echo "$hostname" > /etc/hostname
+echo "Enter the hostname of the server"
+read host_name
+hostnamectl set-hostname $host_name
 sleep 2
 
 # Update repos and groups
 echo "### Updating core repos ###"
 sleep 2
-sudo dnf groupinstall -y "Fedora Server Edition" && \
-sudo dnf groupinstall -y "Container Management" && \
-sudo dnf groupinstall -y "Headless Management" && \
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && \
+dnf groupinstall -y "Fedora Server Edition" && \
+dnf groupinstall -y "Container Management" && \
+dnf groupinstall -y "Headless Management" && \
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && \
 sleep 2
 
 # Install base utilities
 echo "Installing base system utilities"
-sudo dnf install -y \
+dnf install -y \
 cockpit-pcp \
 cockpit-podman \
 cockpit-machines \
@@ -33,23 +36,23 @@ sleep 2
 
 # Enable system services
 echo "Enabling basic system services"
-sudo systemctl enable --now libvirtd && \
-sudo systemctl enable --now cockpit.socket && \
-sudo systemctl enable --now podman.socket &&\
-sudo firewall-cmd --permanent --zone=public --add-port=9090/tcp #Open cockpit port
+systemctl enable --now libvirtd && \
+systemctl enable --now cockpit.socket && \
+systemctl enable --now podman.socket &&\
+firewall-cmd --permanent --zone=public --add-port=9090/tcp #Open cockpit port
 sleep 2
 
 # Install Tailscale
 echo "Installing Tailscale"
-sudo dnf config-manager --add-repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo && \
-sudo dnf install tailscale -y && \
-sudo systemctl enable --now tailscaled && \
-sudo tailscale up && \
+dnf config-manager --add-repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo && \
+dnf install tailscale -y && \
+systemctl enable --now tailscaled && \
+tailscale up && \
 sleep 2
 
 # Update and reboot
 echo "Update everyrhing"
-sudo dnf update -y && \
+dnf update -y && \
 echo "Time to reboot"
 sleep 2
-sudo shutdown -r now
+shutdown -r now
